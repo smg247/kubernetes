f7cdb220f01b2127dab73d7c04f1e9b744f8624a
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-08-24 15:13:04 +0200

    UPSTREAM: <carry>: skip posting failures to aggregated APIs to avoid getting false positives until the server becomes ready
    the availability checks depend on fully initialized SDN
    OpenShift carries a few reachability checks that affect /readyz protocol
    we skip posting failures to avoid getting false positives until the server becomes ready
    
    UPSTREAM: <carry>: skip posting failures to aggregated APIs to avoid getting false positives until the server becomes ready
    
    marks availability of the server before checking the aggregate APIs
    as it can change as we are running the checks.
    in that case, skip posting failures to avoid false positives.
    
    note on the next rebase please squash with the previous commit
    
    UPSTREAM: <carry>: expose HasBeenReady lifecycle signal
    
    OpenShift-Rebase-Source: 8558e889333

--- staging/src/k8s.io/kube-aggregator/pkg/controllers/status/remote/remote_available_controller_test.go
+++ staging/src/k8s.io/kube-aggregator/pkg/controllers/status/remote/remote_available_controller_test.go
@@ -148,6 +148,9 @@ func setupAPIServices(t T, apiServices []runtime.Object) (*AvailableConditionCon
 		}
 	}
 
+	alwaysReadyChan := make(chan struct{})
+	close(alwaysReadyChan)
+
 	c := AvailableConditionController{
 		apiServiceClient:    fakeClient.ApiregistrationV1(),
 		apiServiceLister:    listers.NewAPIServiceLister(apiServiceIndexer),
@@ -161,7 +164,8 @@ func setupAPIServices(t T, apiServices []runtime.Object) (*AvailableConditionCon
 			workqueue.NewTypedItemExponentialFailureRateLimiter[string](5*time.Millisecond, 30*time.Second),
 			workqueue.TypedRateLimitingQueueConfig[string]{Name: "AvailableConditionController"},
 		),
-		metrics: availabilitymetrics.New(),
+		metrics:      availabilitymetrics.New(),
+		hasBeenReady: alwaysReadyChan,
 	}
 	for _, svc := range apiServices {
 		c.addAPIService(svc)
@@ -439,6 +443,8 @@ func TestSync(t *testing.T) {
 				w.WriteHeader(tc.backendStatus)
 			}))
 			defer testServer.Close()
+			alwaysReadyChan := make(chan struct{})
+			close(alwaysReadyChan)
 
 			c := AvailableConditionController{
 				apiServiceClient:           fakeClient.ApiregistrationV1(),
@@ -448,6 +454,7 @@ func TestSync(t *testing.T) {
 				serviceResolver:            &fakeServiceResolver{url: testServer.URL},
 				proxyCurrentCertKeyContent: func() ([]byte, []byte) { return emptyCert(), emptyCert() },
 				metrics:                    availabilitymetrics.New(),
+				hasBeenReady:               alwaysReadyChan,
 			}
 			err = c.sync(tc.apiServiceName)
 			if tc.expectedSyncError != "" {
